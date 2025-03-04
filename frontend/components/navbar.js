84eaf5379b731f152882be7e1c3f8762e201bd67
Ritu <singhritukumari192004@gmail.com>
2025-03-04 22:10:05 +0530

    landing page second part

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -1,14 +1,22 @@
+"use client";
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Menu, X } from 'lucide-react';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <nav className="flex justify-between items-center p-3 bg-black text-white w-3/5 m-auto rounded-full ">
+    <nav className="flex justify-between items-center p-3 bg-black text-white w-full lg:w-3/5 m-auto rounded-full">
+      {/* Logo */}
       <div className="flex items-center">
         <Image src="/logo.png" alt="Collabnest Logo" width={50} height={50} />
         <span className="ml-2 font-bold">Collabnest</span>
       </div>
-      <div className="space-x-6">
+
+      
+      <div className="hidden lg:flex space-x-6">
         <Link href="#">Product</Link>
         <Link href="#">Solution</Link>
         <Link href="#">Pricing</Link>
@@ -16,7 +24,27 @@ const Navbar = () => {
         <Link href="#">Blogs</Link>
         <Link href="#">Cart (0)</Link>
       </div>
-      <button className="px-4 py-2 bg-white text-black rounded-full">Login</button>
+
+      
+      <button className="hidden lg:block px-4 py-2 bg-white text-black rounded-full">Login</button>
+
+      
+      <button className="lg:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <X className="w-8 h-8" /> : <Menu className="w-8 h-8" />}
+      </button>
+
+      
+      {menuOpen && (
+        <div className="absolute top-16 left-0 w-full bg-black text-white flex flex-col items-center space-y-4 py-4 lg:hidden">
+          <Link href="#" onClick={() => setMenuOpen(false)}>Product</Link>
+          <Link href="#" onClick={() => setMenuOpen(false)}>Solution</Link>
+          <Link href="#" onClick={() => setMenuOpen(false)}>Pricing</Link>
+          <Link href="#" onClick={() => setMenuOpen(false)}>About Us</Link>
+          <Link href="#" onClick={() => setMenuOpen(false)}>Blogs</Link>
+          <Link href="#" onClick={() => setMenuOpen(false)}>Cart (0)</Link>
+          <button className="px-4 py-2 bg-white text-black rounded-full" onClick={() => setMenuOpen(false)}>Login</button>
+        </div>
+      )}
     </nav>
   );
 };
