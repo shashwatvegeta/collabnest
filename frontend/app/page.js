97f23cd12ad0fe002d55528cec59cef75946c0f8
HemantKumar01 <ytbhemant@gmail.com>
2025-03-14 20:43:09 +0530

    microsoft auth done

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,40 +1,15 @@
-import Image from 'next/image';
-import Navbar from '@/components/navbar';
-import HowItWorks from '@/components/HowItWorks';
+"use client";
 
-import CollabNestApp from '@/components/landing';
-export default function Home() {
+import { MsalProvider } from "@azure/msal-react";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { msalConfig } from "./authConfig";
+import Home from "./home";
+
+export const msalInstance = new PublicClientApplication(msalConfig);
+export default function MSAuthWrapper() {
   return (
-    <div>
-
-      <div className="relative h-screen bg-cover bg-center p-4 flex flex-col items-center" style={{ backgroundImage: 'url(/landingpage_bg.png)' }}>
-        <Navbar />
-
-        <div className="text-center text-white mt-16 px-6">
-          <h1 className="text-4xl md:text-6xl font-bold">COLLABNEST</h1>
-          <p className="mt-4 text-lg">Bridge the Gap Between Learning & Implementation</p>
-
-          <div className="mt-6 space-x-4">
-            <button className="px-6 py-3 border border-white rounded-full">Join As Student</button>
-            <button className="px-6 py-3 border border-white rounded-full">Join As Mentor</button>
-          </div>
-
-
-          <div className="flex justify-center mt-16">
-            <Image src="/laptop_image.png" alt="Featured Image" width={500} height={500} className="rounded-lg w-full max-w-md md:max-w-lg" />
-          </div>
-        </div>
-      </div>
-
-
-      <div className="mt-20 flex justify-center">
-        <HowItWorks />
-      </div>
-      <div className="mt-20 ">
-        <CollabNestApp />
-      </div>
-
-
-    </div>
+    <MsalProvider instance={msalInstance}>
+      <Home></Home>
+    </MsalProvider>
   );
 }
