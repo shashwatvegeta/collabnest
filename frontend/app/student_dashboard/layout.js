eab04193188ba451486aa7cde5cfae757e7c0eca
helix <abhinavdatta1@gmail.com>
2025-03-18 22:19:27 +0530

    Possible fix for bg

--- frontend/app/student_dashboard/layout.js
+++ frontend/app/student_dashboard/layout.js
@@ -2,7 +2,7 @@ import Sidebar from "@/components/sidebar";
 
 export default function DashboardLayout({ children }) {
 	return (
-		<div className="bg-gradient-to-r from-[#171727] via-[#1f1e37] to-[#19182a] flex">
+		<div className="bg-gradient-to-r from-[#171727] via-[#1f1e37] to-[#19182a] flex h-screen">
 			<Sidebar />
 			<div className="pl-32">{children}</div>
 		</div>
